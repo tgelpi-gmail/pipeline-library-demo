abf521db2475a4e24c3db301821c2917f94df302
Ted Gelpi <tgelpi@gmail.com>
2022-04-11 09:41:31 -0400

    Create defaultUtilityPipeline.groovy

---
+++ vars/defaultUtilityPipeline.groovy
@@ -0,0 +1,41 @@
+// vars/defaultUtilityPipeline.groovy
+// This pipeline requires no parameters as input.
+ 
+ 
+def call(Map pipelineParams) {
+    pipeline {
+        agent none
+ 
+        stages {
+            stage('Build and Unit test') {
+                agent { label 'maven' }
+                steps {
+                    script {
+                        module_Maven('clean verify')
+                    }
+                }
+                post {
+                    always {
+                        junit testResults: '**/target/surefire-reports/*.xml', allowEmptyResults: false
+                    }
+                }
+            }
+            stage('Publish to Nexus') {
+                agent { label 'maven' }
+                steps {
+                    script {
+                        echo "This is where we publish to Nexus"
+                        module_Artifact.publish()
+                    }
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    module_Notification.sendEmail(currentBuild.result)
+                }
+            }
+        }
+    }
+}
